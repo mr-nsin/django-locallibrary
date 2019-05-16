8f912357d88757bd3fe76fc3cf6d14beab8db094
Nitin Singhal <singhal.nitin94@gmail.com>
2019-05-16 07:36:29 +0530

    enabling static files

--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import RedirectView
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('catalog/', include('catlog.urls')),
     path('', RedirectView(url='/catlog/', permanent=True))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
