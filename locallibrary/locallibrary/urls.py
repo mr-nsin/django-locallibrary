076a59dd099a5be6b756386b4513559633e62be1
Nitin Singhal <singhal.nitin94@gmail.com>
2019-05-16 07:15:58 +0530

    Redirect our base url to catalog app

--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('catalog/', include('catlog.urls'))
+    path('catalog/', include('catlog.urls')),
+    path('', RedirectView(url='/catlog/', permanent=True))
 ]
